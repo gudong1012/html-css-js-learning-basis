4cb0d47bff898601bf82153d63764e8a881cb1e1
小咕咚 <122521272+gudong1012@users.noreply.github.com>
2024-07-31 22:38:47 +0800

    Update script.js

--- Countdown Timer/script.js
+++ Countdown Timer/script.js
@@ -3,10 +3,16 @@ const hoursEl = document.getElementById("hours");
 const minsEl = document.getElementById("mins");
 const secondsEl = document.getElementById("seconds");
 
-const newYears = "1 Jan 2025";
+function getNextNewYear() {
+    const currentDate = new Date();
+    const currentYear = currentDate.getFullYear();
+    const nextYear = currentYear + 1;
+    return new Date(`1 Jan ${nextYear}`);
+}
+
 
 function countdown() {
-    const newYearsDate = new Date(newYears);
+    const newYearsDate = getNextNewYear();
     const currentDate = new Date();
     
     const totalSeconds = (newYearsDate - currentDate) / 1000;
